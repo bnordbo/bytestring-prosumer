d13d6032ddd306447dcecdff6404029b10bb1e26
Bjørn Nordbø <bjorn@wearezeta.com>
2014-05-03 10:56:02 +0200

    Added most of the remaining instances + tests
    The Text arbitrary is way too naive still, and is going to blow up every now and then.

--- src/Data/ByteString/From.hs
+++ src/Data/ByteString/From.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Data.ByteString.From (FromByteString(..), fromByteString) where
 
-import           Control.Applicative
-import           Control.Error
-import           Data.Attoparsec.ByteString          (parse)
+import           Control.Applicative                     ((<$>), (<|>))
+import           Data.Attoparsec.ByteString              (parse)
 import           Data.Attoparsec.ByteString.Char8
-import           Data.ByteString                     (ByteString)
-import qualified Data.ByteString.Char8            as C
-import           Data.Either
+import           Data.ByteString                         (ByteString)
+import           Data.ByteString.Char8                   (empty, unpack)
+import           Data.Int
+import           Data.Text                               (Text)
+import           Data.Text.Encoding                      (decodeUtf8)
+import           Data.Word
+import           Prelude                          hiding (takeWhile)
 
 class FromByteString a where
     parser :: Parser a
@@ -17,11 +22,47 @@ fromByteString :: FromByteString a => ByteString -> Maybe a
 fromByteString = either (const Nothing) Just . runParser parser
 
 runParser :: Parser a -> ByteString -> Either String a
-runParser p b = case (feed (parse p b) C.empty) of
+runParser p b = case (feed (parse p b) empty) of
     Done "" r  -> Right r
     Done t r   -> Left $ "Trailing input: " ++ show t
     Fail t _ e -> Left $ "Parsing failed before '" ++ show t ++ "': " ++ e
     Partial c  -> Left   "Unexpected result: Partial"
 
+instance FromByteString ByteString where
+    parser = takeByteString
+
+instance FromByteString String where
+    parser = unpack <$> takeByteString
+
+instance FromByteString Text where
+    parser = decodeUtf8 <$> takeByteString
+
 instance FromByteString Int where
     parser = signed decimal <|> fail "Invalid Int"
+
+instance FromByteString Int8 where
+    parser = signed decimal <|> fail "Invalid Int8"
+
+instance FromByteString Int16 where
+    parser = signed decimal <|> fail "Invalid Int16"
+
+instance FromByteString Int32 where
+    parser = signed decimal <|> fail "Invalid Int32"
+
+instance FromByteString Int64 where
+    parser = signed decimal <|> fail "Invalid Int64"
+
+instance FromByteString Word where
+    parser = decimal <|> fail "Invalid Word"
+
+instance FromByteString Word8 where
+    parser = decimal <|> fail "Invalid Word8"
+
+instance FromByteString Word16 where
+    parser = decimal <|> fail "Invalid Word16"
+
+instance FromByteString Word32 where
+    parser = decimal <|> fail "Invalid Word32"
+
+instance FromByteString Word64 where
+    parser = decimal <|> fail "Invalid Word64"
--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,64 @@
-import Test.Tasty
-import Test.Tasty.QuickCheck as QC
-
-import Data.ByteString.From
-import Data.ByteString.To
+import           Control.Applicative
+import           Data.ByteString          (ByteString)
+import qualified Data.ByteString.Char8 as C
+import           Data.ByteString.From
+import           Data.ByteString.To
+import           Data.Char
+import           Data.Int
+import           Data.String
+import           Data.Text                (Text)
+import qualified Data.Text             as T
+import           Data.Word
+import           Test.Tasty
+import           Test.Tasty.QuickCheck
 
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Bijective"
-    [ QC.testProperty "Int" $ \x -> fromByteString (toByteString x) == Just (x :: Int)
+    [ testProperty "ByteString" $ \x ->
+          fromByteString (toByteString x) == Just (x :: ByteString)
+    , testProperty "String" $ \x ->
+          fromByteString (toByteString x) == Just (x :: String)
+    , testProperty "Text" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Text)
+    , testProperty "Int" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Int)
+    , testProperty "Int8" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Int8)
+    , testProperty "Int16" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Int16)
+    , testProperty "Int32" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Int32)
+    , testProperty "Int64" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Int64)
+    , testProperty "Word" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Word)
+    , testProperty "Word8" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Word8)
+    , testProperty "Word16" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Word16)
+    , testProperty "Word32" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Word32)
+    , testProperty "Word64" $ \x ->
+          fromByteString (toByteString x) == Just (x :: Word64)
     ]
+
+instance Arbitrary ByteString where
+    arbitrary = C.pack <$> listOf (choose ('\0', '\255'))
+
+newtype UTF8 a = UTF8 a
+
+instance Arbitrary Text where
+    arbitrary = do
+        UTF8 c <- arbitrary
+        return $ T.pack c
+
+instance IsString a => Arbitrary (UTF8 a) where
+    arbitrary = UTF8 . fromString . map chr <$> listOf utf8
+      where
+        utf8 = oneof
+            [ choose (     0,   36095 )
+            , choose ( 57344,   65533 )
+            , choose ( 65536, 1114111 )
+            ]
